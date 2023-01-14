92f929a14c9776bc049d7f5c8eb9d9074d0fc090
juanordone <juan.ordonez89@hotmail.es>
2023-01-14 20:10:55 +0100

    clase de tachar

--- todoList/src/components/ListaItems/ListaItems.jsx
+++ todoList/src/components/ListaItems/ListaItems.jsx
@@ -1,12 +1,17 @@
-export default function ListItems({ item, setState, tachar,index }) {
+export default function ListItems({ item, setState, tachar, index }) {
   return (
     <>
       <li
         onClick={() => tachar(index)}
-        className={`list-group-item d-flex justify-content-between ${item.completed && "list-group-item-dark" } `}
+        className={`list-group-item d-flex justify-content-between ${
+          item.completed && "list-group-item-dark text-decoration-line-through"
+        } `}
       >
         {item.title}
-        <button onClick={(e) => setState(e,item.id)} className="btn btn-danger">
+        <button
+          onClick={(e) => setState(e, item.id)}
+          className="btn btn-danger"
+        >
           {" "}
           X
         </button>
